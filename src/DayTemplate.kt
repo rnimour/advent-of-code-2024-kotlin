e236a8c3e1eaac2a2c1264f1327607d75bc487b3
ruud.nimour <ruud.nimour@iodigital.com>
2024-12-05 17:35:35 +0100

    add day template

---
+++ src/DayTemplate.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput("Day02")
+    part1(input).println()
+    part2(input).println()
+}
