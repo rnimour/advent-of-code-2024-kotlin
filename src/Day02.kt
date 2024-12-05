4c9428ecf746354d4f05c77e59be580eb3d9fa14
ruud.nimour <ruud.nimour@iodigital.com>
2024-12-05 17:52:34 +0100

    day 2,2

--- src/Day02.kt
+++ src/Day02.kt
@@ -8,9 +8,11 @@ fun main() {
         .count()
         .toInt()
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
+    fun part2(input: List<String>): Int = input.stream()
+        .map(String::toInts)
+        .filter(::isSafeDampened)
+        .count()
+        .toInt()
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day02")
@@ -21,9 +23,19 @@ fun main() {
 // a report only counts as safe if both of the following are true:
 //     The levels are either all increasing or all decreasing.
 //     Any two adjacent levels differ by at least one and at most three.
-fun isSafe(ints: List<Int>): Boolean {
-    return (ints.zipWithNext().all { (a, b) -> a > b } ||
-            ints.zipWithNext().all { (a, b) -> a < b }
-            ) &&
-            ints.zipWithNext().all { (a, b) -> abs(a - b) <= 3 }
+fun isSafe(ints: List<Int>): Boolean =
+    (
+        ints.zipWithNext().all { (a, b) -> a > b } ||
+        ints.zipWithNext().all { (a, b) -> a < b }
+    ) &&
+        ints.zipWithNext().all { (a, b) -> abs(a - b) <= 3 }
+
+// if one level can be removed to fix it, it is considered safe
+fun isSafeDampened(ints: List<Int>): Boolean {
+    for (i in ints.indices) {
+        if (isSafe(ints.take(i) + ints.drop(i + 1))) {
+            return true
+        }
+    }
+    return false
 }
