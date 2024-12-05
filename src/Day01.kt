3f441a68c8c34fdac171d6747d491a8d9f470442
ruud.nimour <ruud.nimour@iodigital.com>
2024-12-05 13:20:28 +0100

    day 1,1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,33 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        // make lists one and two
+        val list1 = mutableListOf<Int>();
+        val list2 = mutableListOf<Int>();
+        for (line in input) {
+            val both = line.split(" ")
+                .filter { it != "" }
+                .map(String::toInt)
+                .toList()
+            list1.add(both[0])
+            list2.add(both[1])
+        }
+        // sort them
+        list1.sort()
+        list2.sort()
+        // iterate over both at the same time, incrementing the absolute difference
+        var diff = 0
+        list1.forEachIndexed { index, item ->
+            diff += abs(item - list2[index])
+        }
+
+        return diff
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
