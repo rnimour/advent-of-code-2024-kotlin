fb08ae0fb6c5c26f7faa7e8956cf8936c1361948
ruud.nimour <ruud.nimour@iodigital.com>
2025-01-06 18:39:47 +0100

    day 4,2

--- src/Day04.kt
+++ src/Day04.kt
@@ -48,8 +48,53 @@ fun main() {
         return xmasCount
     }
 
+    fun m_up_and_s_down(lineNumber: Int, columnNumber: Int): Boolean =
+        letters[lineNumber - 1][columnNumber - 1] == 'M' &&
+        letters[lineNumber - 1][columnNumber + 1] == 'M' &&
+        letters[lineNumber + 1][columnNumber - 1] == 'S' &&
+        letters[lineNumber + 1][columnNumber + 1] == 'S'
+
+    fun m_down_and_s_up(lineNumber: Int, columnNumber: Int): Boolean =
+        letters[lineNumber - 1][columnNumber - 1] == 'S' &&
+        letters[lineNumber - 1][columnNumber + 1] == 'S' &&
+        letters[lineNumber + 1][columnNumber - 1] == 'M' &&
+        letters[lineNumber + 1][columnNumber + 1] == 'M'
+
+    fun m_left_and_s_right(lineNumber: Int, columnNumber: Int): Boolean =
+        letters[lineNumber - 1][columnNumber - 1] == 'M' &&
+        letters[lineNumber + 1][columnNumber - 1] == 'M' &&
+        letters[lineNumber - 1][columnNumber + 1] == 'S' &&
+        letters[lineNumber + 1][columnNumber + 1] == 'S'
+
+    fun m_right_and_s_left(lineNumber: Int, columnNumber: Int): Boolean =
+        letters[lineNumber - 1][columnNumber - 1] == 'S' &&
+        letters[lineNumber + 1][columnNumber - 1] == 'S' &&
+        letters[lineNumber - 1][columnNumber + 1] == 'M' &&
+        letters[lineNumber + 1][columnNumber + 1] == 'M'
+
+    fun x_masCountOf(lineNumber: Int, columnNumber: Int): Int {
+        if (lineNumber == 0 || lineNumber == 139 || columnNumber == 0 || columnNumber == 139) {
+            return 0
+        }
+        var x_mases = 0
+        if (m_up_and_s_down(lineNumber, columnNumber)
+            || m_down_and_s_up(lineNumber, columnNumber)
+            || m_left_and_s_right(lineNumber, columnNumber)
+            || m_right_and_s_left(lineNumber, columnNumber)) {
+            x_mases += 1
+        }
+        return x_mases
+    }
+
     fun part2(input: List<String>): Int {
-        return 0
+        readIntoLetters(input)
+        var xmasCount = 0
+        letters.forEachIndexed { lineNumber, line ->
+            line.forEachIndexed { columnNumber, char ->
+                xmasCount += if ( char == 'A' ) x_masCountOf(lineNumber, columnNumber) else 0
+            }
+        }
+        return xmasCount
     }
 
     val input = readInput("Day04")
