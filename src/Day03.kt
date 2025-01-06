857d03ca5c2b3bfa46480785d7eefe2f0c0069b9
ruud.nimour <ruud.nimour@iodigital.com>
2025-01-06 13:20:38 +0100

    day 3

---
+++ src/Day03.kt
@@ -0,0 +1,36 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        val regex = Regex("mul\\((\\d+),(\\d+)\\)")
+        var sum = 0
+        for (string in input) {
+            sum += regex.findAll(string).sumOf {
+                (it.groupValues[1].toInt() * it.groupValues[2].toInt())
+            }
+        }
+        return sum
+    }
+
+    fun part2(input: List<String>): Int {
+        val regex = Regex("(mul\\((\\d+),(\\d+)\\))|(do(n't)?\\(\\))")
+        var sum = 0
+        var doMul = true;
+        for (string in input) {
+            for (element in regex.findAll(string)) {
+                if (element.groupValues[0] == "do()") {
+                    doMul = true
+                } else if (element.groupValues[0] == "don't()") {
+                    doMul = false
+                } else if (doMul) {
+                    sum += (element.groupValues[2].toInt() * element.groupValues[3].toInt())
+                }
+            }
+        }
+        return sum
+    }
+
+    val input = readInput("Day03")
+    part1(input).println()
+    part2(input).println()
+}
+
