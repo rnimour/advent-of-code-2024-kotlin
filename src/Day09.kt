cfb40f3ffa2e25d5bc43082e4a0c169f11ae1b01
rnimour <rudenimour@hotmail.com>
2025-01-20 18:12:50 +0100

    9,1

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,8 +1,10 @@
 private val N = 10000
-private val compressedData = ByteArray(N)
-private val gaps = ByteArray(N)
+private val GAP = (-1).toShort() // use magic value -1 as value indicating gap
+private val compressedData = ShortArray(N)
+private val gaps = ShortArray(N)
 private val unCompressedData = ShortArray(N * 10) // 100k is not so short though... ha ha ha
-private var lastDataIndex = N * 10
+private var lastDataIndex = N * 10 - 1
+private var firstGapIndex = 0
 
 fun main() {
 
@@ -10,38 +12,80 @@ fun main() {
         var dataIndex = 0
         input[0].toCharArray().forEachIndexed { index, c ->
             if (index.isEven()) {
-                compressedData[index / 2] = c.code.toByte()
+                compressedData[index / 2] = c.digitToShort()
+                unCompressedData.fill((index / 2).toShort(), dataIndex, dataIndex + c.digitToShort())
             } else {
-                gaps[index / 2] = c.code.toByte()
+                gaps[index / 2] = c.digitToShort()
+                unCompressedData.fill(GAP, dataIndex, dataIndex + c.digitToShort())
             }
-            unCompressedData.fill(c.code.toShort(), dataIndex, dataIndex + c.code)
-            dataIndex += c.code
+            dataIndex += c.digitToShort()
         }
         lastDataIndex = dataIndex - 1
+        // println("lastDataIndex: $lastDataIndex")
+        firstGapIndex = input[0].toCharArray()[0].digitToInt() // - 1 + 1
     }
 
-    fun thereAreGaps(): Boolean {
-        TODO("Not yet implemented")
-    }
+    fun thereAreGaps(): Boolean = firstGapIndex < lastDataIndex
 
     fun moveOneData() {
-        TODO("Not yet implemented")
+        while (unCompressedData[lastDataIndex] == GAP) {
+            lastDataIndex-- // don't count gaps as data
+        }
+        if (unCompressedData[firstGapIndex] == GAP) {
+            // move one data
+            unCompressedData[firstGapIndex++] = unCompressedData[lastDataIndex]
+            unCompressedData[lastDataIndex--] = GAP
+
+        } else {
+            // oh, advance firstGapIndex to the first -1 in front of the current position
+            while (unCompressedData[firstGapIndex] != GAP) {
+                firstGapIndex++
+            }
+        }
+        // if (firstGapIndex > 49880) {
+        //     println(unCompressedData.sliceArray(49880..49920).joinToString(" "))
+        // }
     }
 
     fun compressData() {
+        // var index = 0
         while (thereAreGaps()) {
             moveOneData()
+            // if (index++ % 10 == 0) {
+            //     println(unCompressedData.joinToString(" ").take(200))
+            // }
+            // if (index > 2000) {return}
         }
     }
 
-    fun part1(input: List<String>): Int {
-        readDataAndGaps(input)
-        compressData()
-
-        return 0
+    fun checksum(): Long {
+        var sum = 0L
+        unCompressedData.forEachIndexed { index, value ->
+            if (value == GAP) {
+                println("lastDataIndex: $lastDataIndex")
+                println("firstGapIndex: $firstGapIndex")
+                println("data around 49900:")
+                println(unCompressedData.sliceArray(49880..49920).joinToString(" "))
+                // println("data around index $index:")
+                // println(unCompressedData.sliceArray(index - 10 ..index + 10).joinToString(" "))
+                return sum
+            }
+            sum += index * value
+        }
+        throw IllegalStateException("shouldn't reach here: were gaps not added to the end?")
     }
 
-    fun part2(input: List<String>): Int {
+    fun part1(input: List<String>): Long {
+        readDataAndGaps(input)
+        compressData()
+        return checksum()
+        // the answer is between
+        // 6339935276572 and
+        // 6340985277474 🥲
+        // 6340197768906 can be the answer
+    }
+
+    fun part2(input: List<String>): Long {
         return 0
     }
 
@@ -49,5 +93,7 @@ fun main() {
     part1(input).println()
     part2(input).println()
 }
+
+private fun Char.digitToShort(): Short = digitToInt().toShort()
 
 private fun Int.isEven(): Boolean = this % 2 == 0
